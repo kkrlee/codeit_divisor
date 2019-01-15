d1831eb647e2d3beccacc6034054306007c7124f
minjae <32402820+kkrlee@users.noreply.github.com>
2019-01-15 11:31:23 -0500

    second upload

--- codeit_divisor.py
+++ codeit_divisor.py
@@ -1,10 +1,11 @@
 i = 1
 count = 0
+n = 120
 
-while i <= 120:
-    if 120 % i == 0:
+while i <= n:
+    if n % i == 0:
         print(i)
         count += 1
     i += 1
 
-print("%d의 약수는 총 %d개입니다" % (120, count))
+print("%d의 약수는 총 %d개입니다" % (n, count))
